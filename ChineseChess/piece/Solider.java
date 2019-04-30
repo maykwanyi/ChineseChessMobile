773cb5402e87e35bf85e74769d0c0976b31b6514
maykwanyi <50119336+maykwanyi@users.noreply.github.com>
2019-04-30 16:14:45 -0400

    Update and rename Solider.cs to Solider.java

---
+++ ChineseChess/piece/Solider.java
@@ -0,0 +1,87 @@
+package com.example.chinesechess;
+
+public class Solider extends Piece {
+    public Solider (boolean isRed, int X, int Y, int id)
+    {
+        this.X = X;
+        this.Y = Y;
+        this.isRed = isRed;
+        this.isEmpty = false;
+        this.id = id;
+    }
+
+    public static boolean checkPos(Board board, int X, int Y, boolean isRed)
+    {
+        Solider s = new Solider(isRed, X, Y, 0);
+
+        if (s.checkCrossed(board))
+            return true;
+        else
+        {
+            if (isRed && Y > 3 && (X == 0 || X == 2 || X == 4 || X == 6 || X == 8))
+                return true;
+            else if (!isRed && Y < 7 && (X == 0 || X == 2 || X == 4 || X == 6 || X == 8))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean checkCrossed (Board board)
+    {
+        if (isRed && Y > board.RIVER_RED_SIDE)
+            return true;
+        else if (isRed && Y < board.RIVER_BLACK_SIDE)
+            return true;
+        return false;
+    }
+
+    public boolean move(int X, int Y, Board board)
+    {
+        List<Point2D> moveList = getMoveList(board);
+
+        if (moveList.Contains(new Point2D(X, Y)))
+        {
+            this.X = X;
+            this.Y = Y;
+            eat(board);
+            board.updatePos(this, X, Y);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Point2D> getMoveList(Board board)
+    {
+        List<Point2D> list = new List<Point2D>();
+
+        if (!checkCrossed(board))
+        {
+            if (isRed)
+                list.Add(new Point2D(X, Y + 1));
+            else
+                list.Add(new Point2D(X, Y + 1));
+        }
+        else
+        {
+            if (isRed)
+            {
+                if (board.isIn(X + 1, Y))
+                    list.Add(new Point2D(X + 1, Y));
+                if (board.isIn(X - 1, Y))
+                    list.Add(new Point2D(X - 1, Y));
+                if (board.isIn(X, Y + 1))
+                    list.Add(new Point2D(X + 1, Y));
+            }
+            else
+            {
+                if (board.isIn(X + 1, Y))
+                    list.Add(new Point2D(X + 1, Y));
+                if (board.isIn(X - 1, Y))
+                    list.Add(new Point2D(X - 1, Y));
+                if (board.isIn(X, Y - 1))
+                    list.Add(new Point2D(X, Y - 1));
+            }
+        }
+        return list;
+    }
+}
